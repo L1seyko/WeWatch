3988f6936d87e2ae60dd88dc896954e06e4267bc
Natalia <kovalchuk_natalya2000@mail.ru>
2024-03-07 15:18:44 +0700

    корутины

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,19 @@
+import org.gradle.internal.impldep.com.jcraft.jsch.ConfigRepository.defaultConfig
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
 }
 
 android {
     namespace = "com.hfad.wewatch"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.hfad.wewatch"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -33,15 +36,41 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    //noinspection GradleCompatible
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    //room
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
+    //noinspection KaptUsageInsteadOfKsp
+    kapt("androidx.room:room-compiler:2.6.1")
+    testImplementation("androidx.room:room-testing:2.6.1")
+    //lifecycle
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+    //coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+    //retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    //picasso
+    implementation("com.squareup.picasso:picasso:2.71828")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
--- app/src/main/java/com/hfad/wewatch/MainActivity.kt
+++ app/src/main/java/com/hfad/wewatch/MainActivity.kt
@@ -1,11 +1,164 @@
 package com.hfad.wewatch
-
-import androidx.appcompat.app.AppCompatActivity
+//этот вариант нормальный, удаляет, но не обновляет добавленное
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.*
+import android.view.View.*
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.*
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.hfad.wewatch.model.LocalDataSource
+import com.hfad.wewatch.model.Movie
+import com.hfad.wewatch.network.RetrofitClient
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.toList
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var moviesRecyclerView: RecyclerView
+    private var adapter: MainAdapter? = null
+    private lateinit var fab: FloatingActionButton
+    private lateinit var noMoviesLayout: LinearLayout
+
+    private lateinit var dataSource: LocalDataSource
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + Job())
+
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getMyMoviesList()
+        setupViews()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dataSource = LocalDataSource(application)
+        getMyMoviesList()
+    }
+    override fun onResume() {
+        super.onResume()
+        dataSource = LocalDataSource(application)
+        getMyMoviesList()
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.e(TAG, "stopped")
+        coroutineScope.cancel() // Cancel coroutine scope
+    }
+
+    private fun setupViews() {
+        moviesRecyclerView = findViewById(R.id.movies_recyclerview)
+        moviesRecyclerView.layoutManager = LinearLayoutManager(this)
+        fab = findViewById(R.id.fab)
+        noMoviesLayout = findViewById(R.id.no_movies_layout)
+        supportActionBar?.title = "Movies to Watch"
+    }
+
+    private fun getMyMoviesList() {
+        coroutineScope.launch {
+            try {
+                dataSource.allMovies.collect { movieList ->
+                    displayMovies(movieList)
+                }
+            } catch (e: Exception) {
+                Log.e(TAG, "Error: $e")
+                displayError("Error fetching movie list")
+            }
+        }
+    }
+
+    private val observer: FlowCollector<List<Movie>>
+        get() = object : FlowCollector<List<Movie>> {
+
+            override suspend fun emit(value: List<Movie>) {
+                displayMovies(value)
+            }
+        }
+
+    private fun displayMovies(movieList: List<Movie>?) {
+        if (movieList.isNullOrEmpty()) {
+            Log.d(TAG, "No movies to display")
+            moviesRecyclerView.visibility = INVISIBLE
+            noMoviesLayout.visibility = VISIBLE
+        } else {
+            adapter = MainAdapter(movieList, this@MainActivity)
+            moviesRecyclerView.adapter = adapter
+
+            moviesRecyclerView.visibility = VISIBLE
+            noMoviesLayout.visibility = INVISIBLE
+        }
+    }
+
+    //fab onClick
+    fun goToAddMovieActivity(v: View) {
+        val myIntent = Intent(this@MainActivity, AddMovieActivity::class.java)
+        startActivityForResult(myIntent, ADD_MOVIE_ACTIVITY_REQUEST_CODE)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == ADD_MOVIE_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            // Обновить список фильмов
+            getMyMoviesList()
+            showToast("Movie successfully added.")
+        }
+        else {
+            displayError("Movie could not be added.")
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.deleteMenuItem) {
+            val adapter = this.adapter
+            if (adapter != null) {
+                for (movie in adapter.selectedMovies) {
+                    coroutineScope.launch {
+                        try {
+                            withContext(Dispatchers.IO) {
+                                dataSource.delete(movie)
+                            }
+                            showToast("Movies deleted")
+                        } catch (e: Exception) {
+                            Log.e(TAG, "Error deleting movies: $e")
+                            displayError("Error deleting movies")
+                        }
+                    }
+                }
+                if (adapter.selectedMovies.size == 1) {
+                    showToast("Movie deleted")
+                } else if (adapter.selectedMovies.size > 1) {
+                    showToast("Movies deleted")
+                }
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun showToast(str: String) {
+        Toast.makeText(this@MainActivity, str, Toast.LENGTH_LONG).show()
+    }
+
+    fun displayError(e: String) {
+        showToast(e)
+    }
+
+    companion object {
+        const val ADD_MOVIE_ACTIVITY_REQUEST_CODE = 1
     }
 }
